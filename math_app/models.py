d778153de91d53dd6ebe089b78e1f7843a7c1f9a
AnastasiiaBelianskaia <anastasiiabelianskaia@gmail.com>
2023-03-28 17:00:39 +0300

    Logs model+Middleware

--- math_app/models.py
+++ math_app/models.py
@@ -8,3 +8,21 @@ class Person(models.Model):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+
+class Logs(models.Model):
+    GET = 'GET'
+    POST = 'POST'
+    request_type_choices = [
+        (GET, 'GET'),
+        (POST, 'POST'),
+    ]
+
+    path = models.CharField(max_length=200)
+    method = models.CharField(max_length=4, choices=request_type_choices)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.CharField(max_length=100)
+    data = models.JSONField()
+
+    def __str__(self):
+        return f'{self.user} {self.data}'
